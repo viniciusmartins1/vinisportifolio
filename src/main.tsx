6d984a5af031839956f5dc03aaea0cfad2bcd35c
vinicius.gmartins <vm3090474@gmail.com>
2026-08-01 19:08:49 +1200

    fix: change in imports of the styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { I18nextProvider } from "react-i18next";
+import "./index.css";
 
 import { i18n } from "./i18n";
 import App from "./App.tsx";
